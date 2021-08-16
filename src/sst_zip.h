5d7d638d2c620fb15f4c9b597010adf27639a123
Alexander Klingenbeck <mode7@gmx.net>
2021-08-16 13:40:49 +0200

    Add memory support for reading zip-files

--- src/sst_zip.h
+++ src/sst_zip.h
@@ -7,6 +7,7 @@
 #include "error.h"
 
 sst_ErrorCode sst_zip_readfile(const char* zipPath, const char* path, unsigned char** out_str, size_t* out_size);
+sst_ErrorCode sst_zip_readfile_mem(const char* memory, size_t size, const char* path, unsigned char** out_str, size_t* out_size);
 bool sst_zip_isArchive(const char* zipPath);
 
 #endif
